a0d0b708a1c628b56b4b584a505fac1daf958f27
Shivam7507 <110653564+Shivam7507@users.noreply.github.com>
2024-02-29 14:58:33 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*.liquid",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
